4a69371910329ed03805dcd4571835322cd16e86
bhagathreddy123 <katta_reddy04@yahoo.com>
2015-11-04 23:30:54 +0530

    restrict user for adding cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :products
-  get 'cart/index'
-get '/cart' => "cart#index"
-get '/cart/clear' => "cart#clearcart"
-get "/cart/:id" => "cart#add"
-  get 'page/home'
+	devise_for :users
+	resources :products
+	get 'cart/index'
+	get '/cart' => "cart#index"
+	get '/cart/clear' => "cart#clearcart"
+	get "/cart/:id" => "cart#add"
+	get 'page/home'
 
-  get 'page/about'
-
-  get 'page/faqs'
-
-  get 'page/contact'
-
- root "page#home"
+	get 'page/about'
+	get 'page/faqs'
+	get 'page/contact'
+	root "page#home"
 end
